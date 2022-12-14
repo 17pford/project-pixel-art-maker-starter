c8bc4c1ea54547e5aac15c5d3ee51a9ff16c60b9
17pford <119892159+17pford@users.noreply.github.com>
2022-12-14 13:43:46 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -4,11 +4,11 @@ var color = document.getElementById("colorPicker");
 var sizePicker = document.getElementById("sizePicker");
 var height = document.getElementById("inputHeight");
 var width = document.getElementById("inputWidth");
-var toggle = document.getElementById("gridToggle");
-var save = document.getElementById("saveArt");
+
 
 //function to create grid and assign event listeners to all cells on creation
-function makeGrid(height, width) {
+function makeGrid(e) {
+    e.preventDefault();
     for (let y = 0; y < height; y++) {
         let row = canvas.insertRow(y);
         for (let x = 0; x < width; x++) {
@@ -24,17 +24,6 @@ function makeGrid(height, width) {
     }
 }
 
-//function to toggle on/off grid within the design canvas
-toggle.addEventListener("click", function() {
-    var elementsTd = document.getElementsByTagName("td");
-    var elementsTr = document.getElementsByTagName("tr");
-    for (var i=0; i<elementsTd.length; ++i) {
-        elementsTd[i].classList.toggle("off");
-    }
-    for (var j=0; j<elementsTr.length; ++j) {
-        elementsTr[j].classList.toggle("off");
-    }
-});
 
 //function that uses the size picker submit to call makeGrid() funtion
 sizePicker.addEventListener("submit", function(evt) {
@@ -45,11 +34,3 @@ sizePicker.addEventListener("submit", function(evt) {
     makeGrid(height.value, width.value);
 });
 
-//function that saves the art as canvas
-save.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    html2canvas(canvas).then(canvas => {
-        document.body.appendChild(canvas)
-    });
-});
-});
