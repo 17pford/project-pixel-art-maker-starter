4fd40ab95ba0ac739f5a813032516130aaee033e
17pford <119892159+17pford@users.noreply.github.com>
2022-12-14 13:56:31 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,4 +1,3 @@
-//imports useful elements, and assigns them to variables
 var canvas = document.getElementById("pixelCanvas");
 var color = document.getElementById("colorPicker");
 var sizePicker = document.getElementById("sizePicker");
@@ -6,8 +5,8 @@ var height = document.getElementById("inputHeight");
 var width = document.getElementById("inputWidth");
 
 
-function makeGrid(e) {
-    e.preventDefault();
+//function to create grid and assign event listeners to all cells on creation
+function makeGrid(height, width) {
     for (let y = 0; y < height; y++) {
         let row = canvas.insertRow(y);
         for (let x = 0; x < width; x++) {
@@ -24,11 +23,11 @@ function makeGrid(e) {
 }
 
 
-sizePicker.addEventListener("submit", function(e) {
-    e.preventDefault();
+//function that uses the size picker submit to call makeGrid() funtion
+sizePicker.addEventListener("submit", function(evt) {
+    evt.preventDefault();
     while (canvas.hasChildNodes()) {
         canvas.removeChild(canvas.lastChild);
     }
     makeGrid(height.value, width.value);
 });
-
